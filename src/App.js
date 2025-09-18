e2b29e8c07410454811bb9e93c7d5274f307e47e
Nico <nico_zeuss@icloud.com>
2025-09-18 15:41:15 +0200

    Reload Fix

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import Navbar from './Navbar';
 import Slideshow from './Slideshow';
 import About from './about/About.js';
 import Projects from './projects/Projects.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { HashRouter, Routes, Route } from 'react-router-dom'; 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-      <Navbar />
-      <div id="bg"></div>
-      {/* <Slideshow /> */}
-      <Routes>
-        <Route path="/" element={<Slideshow />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </div>
-    </Router>
+    <HashRouter>
+
+        <div className="App">
+          <Navbar />
+          <div id="bg"></div>
+          {/* <Slideshow /> */}
+          <Routes>
+            <Route path="/" element={<Slideshow />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </div>
+    </HashRouter>
+    
     
   );
 }
